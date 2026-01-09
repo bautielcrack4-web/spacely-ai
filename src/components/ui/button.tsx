0863878806d15b19d90ff347495968af3fffb61c
Bauti <bauti@example.com>
2026-01-09 17:52:54 -0300

    fix(ui): add missing curly braces around props spread in button component

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -48,10 +48,10 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
             <Comp
                 className={cn(buttonVariants({ variant, size, className }))}
                 ref={ref}
-        ...props
-    />
-    )
-  }
+                {...props}
+            />
+        )
+    }
 )
 Button.displayName = "Button"
 
